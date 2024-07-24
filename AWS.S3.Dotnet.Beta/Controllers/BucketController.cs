a526e70f20c48ea75a0670aafe9b3eebfaf4438e
pasindu dissanayake <123033890+maduradevDotNet@users.noreply.github.com>
2024-07-25 02:09:41 +0530

    Fully Crud

---
+++ AWS.S3.Dotnet.Beta/Controllers/BucketController.cs
@@ -0,0 +1,44 @@
+﻿using Amazon.S3;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWS.S3.Dotnet.Beta.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BucketController : ControllerBase
+    {
+        private readonly IAmazonS3 _amazonS3;
+
+        public BucketController(IAmazonS3 amazonS3)
+        {
+            _amazonS3 = amazonS3;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBucketAsync(string bucketName)
+        {
+            var bucketExists = await Amazon.S3.Util.AmazonS3Util.DoesS3BucketExistV2Async(_amazonS3, bucketName);
+            if (bucketExists) return BadRequest($"Bucket {bucketName} already exists.");
+            await _amazonS3.PutBucketAsync(bucketName);
+            return Created("buckets", $"Bucket {bucketName} created.");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllBucketAsync()
+        {
+            var data = await _amazonS3.ListBucketsAsync();
+            var buckets = data.Buckets.Select(b => { return b.BucketName; });
+            return Ok(buckets);
+        }
+
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBucketAsync(string bucketName)
+        {
+            await _amazonS3.DeleteBucketAsync(bucketName);
+            return NoContent();
+        }
+    }
+
+}
